7be5846cd09fe4a2472754a118bb9593a802a39a
hazratamin98 <hazratamin98@gmail.com>
2022-05-20 17:05:51 +0500

    api set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import Homepage from "./Pages/Homepage";
+import Coinpage from "./Pages/Coinpage";
+import { makeStyles } from "@material-ui/core/styles";
+import CryptoContext from "./CryptoContext";
+const useStyles = makeStyles({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+});
 
 function App() {
-  return <div className="App">Hello world</div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.App}>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} exact />
+        <Route path="/coins/:id" element={<Coinpage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,61 @@
+import {
+  capitalize,
+  Container,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+
+const useStyles = makeStyles(() => ({
+  banner: {
+    backgroundImage: "url(./banner2.jpg)",
+  },
+  bannerContent: {
+    height: 400,
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: 25,
+    justifyContent: "space-around",
+  },
+  tagline: {
+    display: "flex",
+    height: "40%",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+}));
+
+const Banner = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.banner}>
+      <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              fontFimaly: "Montserrat",
+            }}
+          >
+            Crypto Hunter
+          </Typography>
+          <Typography
+            variant="subtitle2"
+            style={{
+              color: "darkgrey",
+              textTransform: capitalize,
+              fontFimaly: "Montserrat",
+            }}
+          >
+            Get All the Info regrading your favorite Crypto currnecy
+          </Typography>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Banner;
